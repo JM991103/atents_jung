43a39a35afe28c37c04c6c07872d0d438f894713
JM991103 <ljn1103@naver.com>
2022-09-01 14:32:11 +0900

    유니티실습_21

--- 02_Shooting/Assets/Script/Asteroid.cs
+++ 02_Shooting/Assets/Script/Asteroid.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
@@ -11,6 +13,14 @@ public class Asteroid : MonoBehaviour
     public float maxmoveSpeed = 4.0f;
     public float minrotateSpeed = 30.0f;
     public float maxrotateSpeed = 360.0f;
+
+    float lifetime;
+    public float minLifetime = 3.0f;
+    public float maxLifetime = 3.0f;
+
+    public GameObject small;
+    [Range(1,16)]
+    public int splitCount = 3;
 
     public Vector3 direction = Vector3.left;    //운석이 이동할 방향
     float X = -12.0f;
@@ -40,7 +50,7 @@ public class Asteroid : MonoBehaviour
 
         //int rand = Random.Range(0, 100) % 2;
         //sprite.flipX = (rand == 0);
-        int rand = Random.Range(0, 4);      //rand에다가 0(0b_00),1(0b_01),2(0b_10),3(0b_11)중 하나의 숫자를 랜덤으로 준다.
+        int rand = UnityEngine.Random.Range(0, 4);      //rand에다가 0(0b_00),1(0b_01),2(0b_10),3(0b_11)중 하나의 숫자를 랜덤으로 준다.
 
         //rand & 0b_01 : rand의 제일 오른쪽 비트가 0인지 1인지 확인하는 작업
         //((rand & 0b_01) != 0) : rand의 제일 오른쪽 비트가 1이면 true, 0이면 false
@@ -53,10 +63,28 @@ public class Asteroid : MonoBehaviour
         //moveSpeed = Random.Range(2.0f, 4.0f);
         //rotateSpeed = Random.Range(30.0f, 360.0f);
 
-        moveSpeed = Random.Range(minmoveSpeed, maxmoveSpeed);
+        moveSpeed = UnityEngine.Random.Range(minmoveSpeed, maxmoveSpeed);
+        //속도 = 현재값 - 최소값 / 최대값 - 최소값
+        //회전 = 속도 * (회전최대값 - 회전쇠소값) + 최소값
         float ratio = (moveSpeed - minmoveSpeed) / (maxmoveSpeed - minmoveSpeed);
-        rotateSpeed = ratio * (maxrotateSpeed - minmoveSpeed) + maxrotateSpeed;
+        //rotateSpeed = ratio * (maxrotateSpeed - minmoveSpeed) + maxrotateSpeed;
+        rotateSpeed = Mathf.Lerp(minrotateSpeed, maxrotateSpeed, ratio);
+        //Debug.Log($"calc : {rotateSpeed}");
+        //Debug.Log($"calc : {Mathf.Lerp(minrotateSpeed, maxrotateSpeed, ratio)}");
+        //Debug.Log($"calc : {Mathf.Lerp(minrotateSpeed, maxrotateSpeed, ratio)}");
 
+        lifetime = UnityEngine.Random.Range(minLifetime, maxLifetime);
+
+        explosion = transform.GetChild(0).gameObject;
+
+        StartCoroutine(selfCrush());
+    }
+
+    IEnumerator selfCrush()
+    {
+        yield return new WaitForSeconds(lifetime);
+
+        crush();
     }
 
     // Update is called once per frame
@@ -87,12 +115,28 @@ public class Asteroid : MonoBehaviour
         {
             hitPoint--;
             if (hitPoint <= 0)
-            { 
-            explosion.SetActive(true);
-            explosion.transform.parent = null;
+            {
+                explosion.SetActive(true);
+                explosion.transform.parent = null;
+                Destroy(this.gameObject);
 
-            Destroy(this.gameObject);
+                crush();
             }
         }
+    }
+
+    void crush()
+    {
+        explosion.SetActive(true);
+        explosion.transform.parent = null;
+
+        float angleGap = 360.0f / (float)splitCount;    // 작은 운석들의 진행 방향의 사이각
+        float r = UnityEngine.Random.Range(0.0f, 360.0f);   //첫 운석 방향 변화용
+        for (int i = 0; i < splitCount; i++)
+        {
+            Instantiate(small, transform.position, Quaternion.Euler(0, 0, (angleGap * i) + r));
+        }
+
+        Destroy(this.gameObject);
     }
 }
