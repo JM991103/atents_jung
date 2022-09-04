7c42a740a1532b8dc74f5585abe29c7b1a4e0fa4
JM991103 <ljn1103@naver.com>
2022-09-04 23:04:52 +0900

    유니티과제_1

--- 02_Shooting/Assets/Script/PowerUpMove.cs
+++ 02_Shooting/Assets/Script/PowerUpMove.cs
@@ -18,13 +18,21 @@ public class PowerUpMove : MonoBehaviour
         cooldown += Time.deltaTime;
         if (cooldown >= 1.0f)
         {
-        Move = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0).normalized;
+            Move = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0).normalized;
 
-        cooldown = 0.0f;
+            cooldown = 0.0f;
         }
-        transform.Translate(Move * speed * Time.deltaTime);
+        transform.Translate(speed * Time.deltaTime * Move);
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Border"))
+        {
+            Move.y = -Move.y;
+            Move.x = -Move.x;
 
+        }
+    }
 }
 
