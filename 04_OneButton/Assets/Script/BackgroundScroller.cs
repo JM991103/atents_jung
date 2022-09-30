d31424c8a5319ca6c8b7901232bd1ca58acc74fe
JM991103 <ljn1103@naver.com>
2022-09-30 09:30:00 +0900

    유니티2D_flappyBird_05
    백그라운드 스크롤링 완성

--- 04_OneButton/Assets/Script/BackgroundScroller.cs
+++ 04_OneButton/Assets/Script/BackgroundScroller.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float scrollingspeed = 5.0f;
+    public float widht = 7.2f;  // 하늘은 7.2, 바닥은 8.4
+
+    Transform[] bgSlots;
+    float edgPoint;
+
+    private void Awake()
     {
-        
+        //bgSlots를 배경의 트랜스폼으로 채우기
+        bgSlots = new Transform[transform.childCount];
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            bgSlots[i] = transform.GetChild(i);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
+    {
+        edgPoint = transform.position.x - widht * 2.0f;
+    }
+
+    private void Update()
     {
         
+        foreach (var slot in bgSlots)
+        {
+            slot.Translate(scrollingspeed * Time.deltaTime * -transform.right);
+            if (slot.position.x < edgPoint)
+            {
+                slot.Translate(widht * bgSlots.Length * transform.right);
+            }
+        }
     }
+
 }
