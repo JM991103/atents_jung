20763b84890c15825deb7a807b629fff6e9f81b8
JM991103 <ljn1103@naver.com>
2022-10-12 23:08:47 +0900

    Update ScoreUI.cs

--- 04_OneButton/Assets/Scripts/UI/ScoreUI.cs
+++ 04_OneButton/Assets/Scripts/UI/ScoreUI.cs
@@ -7,12 +7,12 @@ public class ScoreUI : MonoBehaviour
 {
     private void Start()
     {
-        GameManager.Inst.OnGameStart += OnGameStart;
-        gameObject.SetActive(false);
+        GameManager.Inst.OnGameStart += OnGameStart;    // 시작할 때 실행될 델리게이트에 함수 연결
+        gameObject.SetActive(false);                    // 이 게임 오브젝트 끄기
     }
 
     private void OnGameStart()
     {
-        gameObject.SetActive(true);
+        gameObject.SetActive(true);                     // 게임이 시작될 때 이 게임오브젝트 켜기
     }
 }
