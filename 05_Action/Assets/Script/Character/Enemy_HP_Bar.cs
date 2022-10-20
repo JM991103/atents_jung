8d12216419a96f7ae69d9546ef47cf16eced8bb0
JM991103 <ljn1103@naver.com>
2022-10-20 14:17:09 +0900

    Action - HP바 빌보드로 만들기

--- 05_Action/Assets/Script/Character/Enemy_HP_Bar.cs
+++ 05_Action/Assets/Script/Character/Enemy_HP_Bar.cs
@@ -19,4 +19,13 @@ public class Enemy_HP_Bar : MonoBehaviour
     {
         fill.localScale = new Vector3(ratio, 1, 1); // 입력받은 비율대로 스케일 조절
     }
+
+    /// <summary>
+    /// 모든 게임 오브젝트의 Update 함수가 호출된 이후에 호출되는 Update함수
+    /// </summary>
+    private void LateUpdate()
+    {
+        //transform.forward = Camera.main.transform.forward;    // 카메라의 forward와 나의 forward를 일치시킴 (빌보드)
+        transform.rotation = Camera.main.transform.rotation;    // 카메라의 회전을 일치시킴
+    }
 }
