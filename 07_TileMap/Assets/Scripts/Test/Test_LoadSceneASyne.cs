d6a46581d607f9138dc3d1f69df7617b00003e3e
JM991103 <ljn1103@naver.com>
2023-01-04 09:19:07 +0900

    TileMap - 테스트용 로딩씬 만들기

--- 07_TileMap/Assets/Scripts/Test/Test_LoadSceneASyne.cs
+++ 07_TileMap/Assets/Scripts/Test/Test_LoadSceneASyne.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,25 @@ using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 public class Test_LoadSceneASyne : TestBase
 {
+    AsyncOperation async;
     protected override void Test1(InputAction.CallbackContext _)
     {
         //SceneManager.LoadScene(1, LoadSceneMode.Additive);
-        SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+        async = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+        async.completed += Async_completed;
+    }
+
+    private void Async_completed(AsyncOperation obj)
+    {
+        Debug.Log("로딩 완료");
+        async = null;
+    }
+
+    private void Update()
+    {
+        if (async != null)
+        {
+            Debug.Log($"");
+        }
     }
 }
