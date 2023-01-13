ffc28b70194ac7de89f2991bd735a7723fb7c84e
JM991103 <ljn1103@naver.com>
2023-01-13 12:04:46 +0900

    Network - 로거 만들기 (진행중)
    스크롤 뷰 적용

---
+++ 08_Network/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    Logger logger;
+    public Logger Logger => logger;
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        logger = FindObjectOfType<Logger>();
+    }
+}
---
+++ 08_Network/Assets/Scripts/Test_Network/Test_Logger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_Logger : TestBase
+{
+    protected override void Test1(InputAction.CallbackContext _)
+    {
+        GameManager.Inst.Logger.Log("123123\n");
+    }
+}
