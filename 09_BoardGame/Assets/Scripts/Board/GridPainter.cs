1118bb77d3e98a070357d6da0512d90193952be0
JM991103 <ljn1103@naver.com>
2023-02-01 10:32:56 +0900

    BoardGame - 그리드 생성 완료

--- 09_BoardGame/Assets/Scripts/Board/GridPainter.cs
+++ 09_BoardGame/Assets/Scripts/Board/GridPainter.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GridPainter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject linePrefab;
+    public GameObject letterPrefab;
+
+    const int gridLineCount = 11;
+
+    private void Awake()
     {
-        
+        DrawGridLine();
+        DrawGridLetter();
     }
 
-    // Update is called once per frame
-    void Update()
+    void DrawGridLine()
     {
-        
+        // 세로 선 그리기(세로 선을 옆으로 반복해서 그리기)
+        for (int i = 0; i < gridLineCount; i++)
+        {
+            GameObject line = Instantiate(linePrefab, transform);
+            LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, new Vector3(i, 0, 1));
+            lineRenderer.SetPosition(1, new Vector3(i, 0, 1 - gridLineCount));
+        }
+
+        // 가로 선 그리기(가로선을 아래로 반복해서 그리기)
+        for (int i = 0; i < gridLineCount; i++)
+        {
+            GameObject line = Instantiate(linePrefab, transform);
+            LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, new Vector3(-1, 0, -i));
+            lineRenderer.SetPosition(1, new Vector3(-1 + gridLineCount, 0, -i));
+        }
+    }
+
+    void DrawGridLetter()
+    {
+        // 알파벳을 가로로 쓰기
+        for (int i = 1; i < gridLineCount; i++)
+        {
+            GameObject letter = Instantiate(letterPrefab, transform);
+            letter.transform.position = new Vector3(i - 0.5f, 0, 0.5f);
+            TextMeshPro text = letter.GetComponent<TextMeshPro>();
+            char alphbet = (char)(64 + i);
+            text.text = alphbet.ToString();
+        }
+
+        // 숫자를 세로로 쓰기
+        for (int i = 0; i < gridLineCount; i++)
+        {
+            GameObject letter = Instantiate(letterPrefab, transform);
+            letter.transform.position = new Vector3(-0.5f, 0, 0.5f - i);
+            TextMeshPro text = letter.GetComponent<TextMeshPro>();
+            text.text = i.ToString();
+
+            if (i > 9)
+            {
+                text.fontSize = 8;
+            }
+        }
     }
 }
