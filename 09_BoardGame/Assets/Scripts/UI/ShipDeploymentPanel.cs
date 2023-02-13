811177abb166b629563c0c6ba92701b1c3225a86
JM991103 <ljn1103@naver.com>
2023-02-13 12:58:33 +0900

    주석 추가

--- 09_BoardGame/Assets/Scripts/UI/ShipDeploymentPanel.cs
+++ 09_BoardGame/Assets/Scripts/UI/ShipDeploymentPanel.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipDeploymentPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 이 패널 아래에 있는 모든 토글 버튼
+    /// </summary>
+    DeploymentToggle[] toggles;
+
+    private void Awake()
     {
-        
+        toggles = GetComponentsInChildren<DeploymentToggle>();
+        foreach (DeploymentToggle toggle in toggles)
+        {
+            toggle.onTogglePress += OnTogglePress;  // 모든 토글 버튼의 델리게이트에 함수 등록
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 토글 버튼이 눌러진 상태가 되었을 때 실행되는 함수
+    /// </summary>
+    /// <param name="toggle">눌러진 토글 버튼</param>
+    private void OnTogglePress(DeploymentToggle toggle)
     {
-        
+        foreach (var others in toggles) 
+        {
+            if (toggle != others)   // 자신을 제외한 나머지 토글 버튼들을
+            {
+                others.UnToggle();  // 눌러진 상태에서 해제
+            }
+        }
     }
 }
